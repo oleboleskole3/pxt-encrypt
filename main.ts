66a61919a7b5b911fc414fbe6b2e1e0b9d26dc9e
oleboleskole3 <43952813+oleboleskole3@users.noreply.github.com>
2021-05-01 00:52:04 +0200

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,49 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace MicroEncrypt {
+    //% block="encrypt %text with key %key"
+    export function encrypt(text : string, key : number) : string {
+        let fail : string = ''
+        const out : string = text.split('').map(char => {
+            if (fail) return ''
+            const charCode = char.charCodeAt(0)
+            if (charCode < 32 || charCode > 126) {
+                fail = char
+                return ''
+            }
+            return String.fromCharCode((charCode - 32 + key) % 93 + 32)
+        }).join('')
+
+        return fail ? 'Unexpected character ' + fail : out
+    }
+
+    //% block="decrypt %text with key %key"
+    export function decrypt(text : string, key : number) : string {
+        let fail : string = ''
+        const out : string = text.split('').map(char => {
+            if (fail) return ''
+            let charCode = char.charCodeAt(0)
+            if (charCode < 32 || charCode > 126) {
+                fail = char
+                return ''
+            }
+            charCode -= key
+
+            while (charCode < 32) {
+                charCode += 93
+            }
+
+            return String.fromCharCode(charCode)
+        }).join('')
+
+        return fail ? 'Unexpected character ' + fail : out
+    }
+}
+
+//32 126
